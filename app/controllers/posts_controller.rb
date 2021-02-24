d575dff5eff0b54c69084da8c1324af45a3961d3
SunnySparks <josediazt6@outlook.com>
2021-02-24 16:16:58 -0600

    Fix spacing rubocop offense

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: %i[show edit update destroy]
 
 
   def index
